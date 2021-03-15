acd6d954ea0e01088a9e124243653aefa67abe13
TreasureMaster <54545559+TreasureMaster@users.noreply.github.com>
2021-03-15 18:51:44 +0300

    comment cleaning

--- flaskr/post.py
+++ flaskr/post.py
@@ -1,20 +1,15 @@
 from os import error
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, current_app, session
+    Blueprint, flash, g, render_template, request
 )
-from werkzeug.exceptions import abort
-from markupsafe import Markup
-
 from flaskr.auth import login_required
 from flaskr.db import get_db
 from flaskr.blog import get_post as get_post_super
 from flaskr import __version__
 
-# from requirements import author_required
 from flaskr.requirements import author_denied
-
-# Пока пробные варианты Flash
 from .widgets.flashes import is_category, pflash
+
 
 # Схема страницы блога INDEX
 bp = Blueprint('post', __name__)
@@ -27,8 +22,7 @@ def view_post(id):
     return render_template(
         'posts/view.html',
         post=post,
-        comments=comments,
-        # likes_widget=g.likes(id)
+        comments=comments
     )
 
 # Лайки для сообщений
@@ -36,17 +30,10 @@ def view_post(id):
 @login_required
 @author_denied(for_widget='likes')
 def set_regard(id, regard):
-    # current_app.logger.info(session.get('_flashes'))
-    # error = 'None'
-    # if is_author(id):
-    #     error = 'The author cannot like his posts.'
     if g.likes.is_regard_set(id):
         pflash("You've already liked it.", 'error')
 
-    # if error is not None:
     if not is_category('error'):
-        # flash(error)
-    # else:
         db = get_db()
         db.execute(
             'INSERT INTO likes (post_id, %s)'
@@ -54,10 +41,10 @@ def set_regard(id, regard):
             (id, g.user['id'])
         )
         db.commit()
+
     return render_template(
         'posts/view.html',
         post=get_post(id),
-        # likes_widget=g.likes(id),
         comments=get_comments(id)
     )
 
@@ -66,7 +53,6 @@ def set_regard(id, regard):
 @login_required
 def write_comment(id):
     error = None
-    # comments = get_comments(id)
     if request.method == 'POST':
         comment = request.form['comment']
 
@@ -97,28 +83,6 @@ def update_comment(id, post):
 
 
 # -------------------------- Вспомогательные функции ------------------------- #
-
-# Проверяет является ли текущий юзер автором данного поста
-# def is_author(post_id):
-#     user_id = get_db().execute(
-#         'SELECT author_id'
-#         ' FROM post'
-#         ' WHERE id = ?',
-#         (post_id,)
-#     ).fetchone()[0]
-#     return g.user is not None and user_id == g.user['id']
-
-# Определяет оставлял ли текущий пользователь какой-либо лайк в данном посте
-# def is_regard_set(post_id):
-#     regard = get_db().execute(
-#         'SELECT *'
-#         ' FROM likes'
-#         ' WHERE post_id = ?'
-#         ' AND (like = ? OR dislike = ?)',
-#         (post_id, g.user['id'], g.user['id'])
-#     ).fetchone()
-
-#     return regard is not None
 
 # Переопределяет get_post из flaskr.blog, добавляя новые значения
 def get_post(post_id):
